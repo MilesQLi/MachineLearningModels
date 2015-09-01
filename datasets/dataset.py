f19828fddc75a0c34f4893e9e6fed9f2986992b0
Li Qi <crazymage@pku.edu.cn>
2015-09-01 18:58:49 +0800

    complete dataset producer and start plotdata

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -1,5 +1,6 @@
 #coding=utf-8
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def classification_dataset_produce(func, input_interval, num):
@@ -7,6 +8,8 @@ def classification_dataset_produce(func, input_interval, num):
     func: real data discriminant function
     input_interval: the interval of every dimension of input data
     num: the number of datas to produce
+    
+    return the data set in the form [[input,output]] which input is array output is number
     '''
     if not isinstance(input_interval, np.ndarray):
         input_interval = np.array(input_interval)
@@ -15,16 +18,33 @@ def classification_dataset_produce(func, input_interval, num):
     
     average = input_interval.mean(1)
     span = input_interval.transpose()[1]-input_interval.transpose()[0]
-    print average,span
     
     dimension = len(input_interval)
 
-    data = np.ndarray((dimension,num))
+    datas = np.ndarray((dimension,num))
     
     for i in range(dimension):
-        data[i] = average[i]
-        data[i] += span[i]*np.random.random(num) - span[i] / 2
+        datas[i] = average[i]
+        datas[i] += span[i]*np.random.random(num) - span[i] / 2
     
-    data = data.transpose()
+    datas = datas.transpose()
     
+    outputs = []
     
+    for data in datas:
+        outputs.append(func(data))
+        
+    return [(data,output) for (data,output) in zip(datas,outputs)]
+    '''    
+    outputs = np.array(outputs)
+    outputs.shape=(outputs.shape[0],1)
+    return np.hstack((datas,outputs))
+    '''
+
+def plot_data(x,y):
+    '''
+    x must be 2 dimension
+    '''
+    a = 5
+    plt.plot(x, y)
+    plt.show()
--- test/test_data_producer.py
+++ test/test_data_producer.py
@@ -1,7 +1,18 @@
 import sys
-sys.path.append('../datasets/')
-from dataset import *
+sys.path.append('..')
+from datasets import *
+
+
+def func(x):
+    if 2*x[1] - 3*x[0] > 23.5:
+        return 1
+    else:
+        return 0
 
 if __name__ == '__main__':
-    a=5
-    classification_dataset_produce(a,[[5,6],[18,22]],10)
+    datas = dataset.classification_dataset_produce(func,[[5,6],[18,22]],10)
+    x = [list(data[0]) for data in datas]
+    y = [data[1] for data in datas]
+    print x,y
+    
+    dataset.plot_data(x, y)
