fb3e34af607e0cc4f87fbfb777b1ecd2a9d23297
Li Qi <crazymage@pku.edu.cn>
2015-11-05 08:51:16 +0800

    kd tree works

--- with_numpy/PCA.py
+++ with_numpy/PCA.py
@@ -12,7 +12,7 @@ class PCA(object):
     
     def train(self,X):
         m,n = X.shape
-        self.mean = np.average(X, axis = 1).reshape(X.shape[0],1)
+        self.mean = np.average(X, axis = 0).reshape(X.shape[1],)
         X_ = X - self.mean
         if m>n:
             U,S,V = svd(X_.transpose())
@@ -27,10 +27,14 @@ class PCA(object):
     def reconstruct(self,X):
         X_ = X - self.mean
         temp1 = np.dot(X_,self.U[:,:self.n_component])
+        #print temp1[:6,:]
         temp2 = np.dot(temp1,self.U[:,:self.n_component].transpose())
         result = temp2 + self.mean   
         print 'mean error:',np.sum((X-result)**2)/(X.shape[0]*X.shape[1])
         return result
+    def construct(self,X):
+        temp = np.dot(X,self.U[:,:self.n_component].transpose())
+        return temp + self.mean
 
 def plot_gallery(title, images, n_col=3, n_row=2):
     '''
@@ -72,6 +76,21 @@ if __name__ == '__main__':
     plt.show()
 
 '''
+#make face
+if __name__ == '__main__':
+    rng = RandomState(0)
+    dataset = fetch_olivetti_faces(shuffle=True, random_state=rng)
+    faces = dataset.data  
+    tima  = 0  
+    pca = PCA(20)
+    pca.train(faces)
+    print faces.shape,pca.mean.shape
+    test_x = (np.random.rand(30,20)-0.5)*20
+    print test_x.shape
+    plot_gallery('recon components', pca.construct(test_x),5,6)
+    plt.show()
+    print tima    
+'''   
 #test success    
 if __name__ == '__main__':
     rng = RandomState(0)
@@ -79,7 +98,7 @@ if __name__ == '__main__':
     faces = dataset.data  
     plot_gallery('origin', faces[:6])
     tima  = 0
-    for i in range(5,50,5):    
+    for i in range(20,50,5):    
         print 'n_comonents:',i
         pca = PCA(i)
         t1 = time.clock()
@@ -89,3 +108,5 @@ if __name__ == '__main__':
         plot_gallery('recon %d components'%i, recon[:6])
     plt.show()
     print tima
+
+'''
--- with_numpy/knn.py
+++ with_numpy/knn.py
@@ -12,10 +12,81 @@ import random
 import csv
 from matplotlib.colors import ListedColormap
 
-#TODO kdtree version is still not implemented
+#TODO use_kdtree version is still not implemented
+
+
+class node(object):
+    def __init__(self,data=None,left=None,right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+class neighbours(object):
+    def __init__(self,target,k,dist):
+        self.target = target
+        self.k = k
+        self.neighbours = []
+        self.largest_distance = 0
+        self.dist = dist
+    def add(self,point):
+        dis = self.dist(point,self.target)
+        for i,e in enumerate(self.neighbours):
+            if i == self.k:
+                return
+            if e[1] > dis:
+                self.neighbours.insert(i, [point,dis])
+                self.neighbours = self.neighbours[:self.k]
+                self.largest_distance = self.neighbours[-1][1]
+                assert len(self.neighbours) <= self.k
+                return
+        self.neighbours.append([point,dis])
+        self.neighbours = self.neighbours[:self.k]
+        self.largest_distance = self.neighbours[-1][1]
+        #print 'len:',len(self.neighbours)
+        assert len(self.neighbours) <= self.k
+    def get_neighbours(self):
+        return [e[0] for e in self.neighbours]
+
+class kdtree(object):
+    def __init__(self,X,dist):
+        self.m = X.shape[1]
+        self.n = X.shape[0]
+        self.root = self.build_tree(list(X),0)
+        self.dist = dist
+    def build_tree(self,X,depth):
+        if X == []:
+            return None
+        dim = depth%self.m
+        X.sort(key = lambda x:x[dim])
+        median = len(X) / 2
+        return node(X[median],self.build_tree(X[:median], depth+1),self.build_tree(X[median+1:], depth+1))
+    def search(self,target,k):
+        def search_it(best_neighbours,root,depth):
+            if root == None:
+                return
+            best_neighbours.add(root.data)
+            dim = depth % self.m
+            if target[dim] < root.data[dim]:
+                near_part = root.left
+                far_part = root.right
+            else:
+                near_part = root.right
+                far_part = root.left
+            search_it(best_neighbours, near_part, depth+1)
+            
+            if np.abs(root.data[dim]-target[dim]) < best_neighbours.largest_distance:
+                search_it(best_neighbours, far_part, depth+1)
+            
+            
+        best_neighbours = neighbours(target,k,self.dist)
+        search_it(best_neighbours,self.root,0)
+        return best_neighbours.get_neighbours()
+
+
+
 
 class KNN(object):
-    def __init__(self,datax,datay,k = 5,dist = "Euclidean",kdtree = True):
+    def __init__(self,datax,datay,k = 5,dist = "Euclidean",use_kdtree = True):
         self.k = k
         if isinstance(dist,str):
             if hasattr(self,dist):
@@ -29,10 +100,9 @@ class KNN(object):
             self.dist = dist
         self.datax = datax
         self.datay = datay
-        self.kdtree = kdtree
-        if kdtree:
-            #TODO
-            pass
+        self.use_kdtree = use_kdtree
+        if use_kdtree:
+            self.kdtree = kdtree(X,self.dist)
         
     def Euclidean(self, x1, x2):
         return np.sqrt(np.sum((x1-x2)**2))
@@ -44,7 +114,7 @@ class KNN(object):
         return (np.sum((x1-x2)**self.dist_rank))**(1/self.dist_rank)
     
     def find_kneighbours(self, x):
-        if not self.kdtree:
+        if not self.use_kdtree:
             dists = []
             for i in range(len(self.datax)):
                 dists.append((i,self.dist(x,self.datax[i])))
@@ -53,6 +123,19 @@ class KNN(object):
             for i in range(self.k):
                 neighbours.append(dists[i][0])
             return neighbours
+        else:
+            return self.kdtree.search(x, self.k)
+    #for compare with kd tree
+    def print_kneighbours(self, x):
+        if not self.use_kdtree:
+            dists = []
+            for i in range(len(self.datax)):
+                dists.append((i,self.dist(x,self.datax[i])))
+            dists.sort(key=operator.itemgetter(1))
+            neighbours = []
+            for i in range(self.k):
+                neighbours.append(dists[i][0])
+            return [self.datax[i] for i in neighbours]
     def predict(self, x):
         neighbours = self.find_kneighbours(x)
         counts = {}
@@ -67,6 +150,29 @@ class KNN(object):
             print i,len(X),'done'
             result.append(self.predict(x))
         return np.array(result)
+
+'''
+if __name__ == '__main__':
+    n_neighbors = 5
+    X,y = datasets.make_moons(220,True,noise=0.21)
+    knn = KNN(X, y, n_neighbors,'Euclidean',False) 
+    kd = kdtree(X,knn.dist)
+    
+    for i in range(X.shape[0]):
+        a = kd.search(X[i], n_neighbors)
+        b = knn.print_kneighbours(X[i])
+        for l,m in zip(a,b):
+            print np.sum(np.abs(l-m))
+    for i in a:
+        print i,knn.dist(X[1],i)
+    print 'second part'
+    b = knn.print_kneighbours(X[1])
+    for i in b:
+        print i,knn.dist(X[1],i)
+    #print kd.search(X[1], n_neighbors) == knn.print_kneighbours(X[1])
+    print X[:10]
+'''
+
 if __name__ == '__main__':
     n_neighbors = 5
     X,y = datasets.make_moons(220,True,noise=0.21)
