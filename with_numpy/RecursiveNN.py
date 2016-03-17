c9800d6c8149bf01eb05b731e21d87b4d5e03096
Li Qi <crazymage@pku.edu.cn>
2016-03-17 20:42:20 +0800

    update

--- with_numpy/RecursiveNN.py
+++ with_numpy/RecursiveNN.py
@@ -19,11 +19,12 @@ class RecursiveNN(object):
     def __init__(self,n_in,n_hid):
         self.n_in = n_in
         self.n_hid = n_hid
-        
-        
+        self.wx = np.array(np.random.uniform(low=-0.012, high=0.012, size=(n_hid,n_in)))
+        self.wh = np.array(np.random.uniform(low=-0.012, high=0.012, size=(n_hid,n_hid)))
+        self.b = np.zeros(n_hid,)  # @UndefinedVariable
         
     def farward(self,x,h):
-        pass
+        return self.w.dot(x)+self.b
     
     def backward(self,delta_h,h):
         pass
