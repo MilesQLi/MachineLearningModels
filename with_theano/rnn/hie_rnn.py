0b0ebc0ff2c8ecb9e787b170f5aaa8945653dbc3
Li Qi <crazymage@pku.edu.cn>
2017-01-17 11:15:31 +0800

    add attention and fix bugs of hie_rnn and rnn_cells.py

--- with_theano/rnn/hie_rnn.py
+++ with_theano/rnn/hie_rnn.py
@@ -34,14 +34,18 @@ class hie_rnn(object):
         if n_h_s == None:
             n_h_s = n_h
         
-        self.sentence_lstm = getattr(rnn_cells, rnn_cell)(n_in, n_h, emb, pre_fix+'sen')
-        self.doc_lstm = getattr(rnn_cells, rnn_cell)(n_h, n_h_s, emb, pre_fix+'doc')
+        self.sentence_lstm = getattr(rnn_cells, rnn_cell)(n_in, n_h, pre_fix+'sen')
+        
+        
+        self.doc_lstm = getattr(rnn_cells, rnn_cell)(n_h, n_h_s, pre_fix+'doc')
         
         self.params = self.sentence_lstm.params+self.doc_lstm.params
         
-        _,hs = self.sentence_lstm.get_sentence_output(self.emb[self.x],self.mask)
+        result = self.sentence_lstm.get_sentence_output(self.emb[self.x],self.mask)
+        hs = result['hs']
         
-        cs_s, hs_s = self.doc_lstm.get_onedoc_output(hs[-1])
+        result = self.doc_lstm.get_onedoc_output(hs[-1])
+        cs_s, hs_s = result['cs'], result['hs']
         self.s_ho = hs_s[-1]
         self.s_co = cs_s[-1]
         
@@ -53,7 +57,11 @@ class hie_rnn(object):
           
         self.to_sentence = to_sentence
         
-        cs_s, hs_s = self.doc_lstm.get_sentence_output(self.to_sentence, self.s_mask)
+        result = self.doc_lstm.get_sentence_output(self.to_sentence, self.s_mask)
+        cs_s, hs_s = result['cs'], result['hs']
          
         self.h_o = hs_s[-1]
         self.c_o = cs_s[-1]      
+        
+        self.h_os = hs_s
+        
--- with_theano/rnn/rnn_cells.py
+++ with_theano/rnn/rnn_cells.py
@@ -7,10 +7,15 @@ import theano
 
 
 class lstm(object):
-    def __init__(self, n_in, n_h, pre_fix):     
+    def __init__(self, n_in, n_h, pre_fix, attention = 0):     
+        
+        #attention 1 Interactive Attention for Neural Machine Translation normal attention also A Hierarchical Neural Autoencoder for Paragraphs and Documents
+        #attention 2 Interactive Attention for Neural Machine Translation improved attention
+        #attention 3 Interactive Attention for Neural Machine Translation interactive attention
         
         self.n_in = n_in
         self.n_h = n_h
+        self.attention = attention
         
         self.pre_fix = pre_fix
         self.name = pre_fix + 'lstm'   
@@ -28,6 +33,10 @@ class lstm(object):
         self.b_i = theano.shared(np.array(np.random.uniform(low=-bound_h, high=bound_h, size=(n_h,)), dtype=theano.config.floatX), name=self.name+'b_i', borrow=True)  # @UndefinedVariable
         self.params.append(self.b_i)
         
+        w_xi = self.w_xi
+        w_hi = self.w_hi
+        b_i = self.b_i
+        
         self.w_xf = theano.shared(np.array(np.random.uniform(low=-bound_x, high=bound_x, size=(n_in, n_h)), dtype=theano.config.floatX), name=self.name+'w_xf', borrow=True)  # @UndefinedVariable
         self.params.append(self.w_xf)
         self.w_hf = theano.shared(np.array(np.random.uniform(low=-bound_h, high=bound_h, size=(n_h, n_h)), dtype=theano.config.floatX), name=self.name+'w_hf', borrow=True)  # @UndefinedVariable
@@ -37,12 +46,21 @@ class lstm(object):
         self.b_f = theano.shared(np.array(np.random.uniform(low=-bound_h, high=bound_h, size=(n_h,)), dtype=theano.config.floatX), name=self.name+'b_f', borrow=True)  # @UndefinedVariable
         self.params.append(self.b_f)
         
+        w_xf = self.w_xf
+        w_hf = self.w_hf
+        b_f = self.b_f
+        
+        
         self.w_xc = theano.shared(np.array(np.random.uniform(low=-bound_x, high=bound_x, size=(n_in, n_h)), dtype=theano.config.floatX), name=self.name+'w_xc', borrow=True)  # @UndefinedVariable
         self.params.append(self.w_xc)
         self.w_hc = theano.shared(np.array(np.random.uniform(low=-bound_h, high=bound_h, size=(n_h, n_h)), dtype=theano.config.floatX), name=self.name+'w_hc', borrow=True)  # @UndefinedVariable
         self.params.append(self.w_hc)
         self.b_c = theano.shared(np.array(np.random.uniform(low=-bound_h, high=bound_h, size=(n_h,)), dtype=theano.config.floatX), name=self.name+'b_c', borrow=True)  # @UndefinedVariable       
         self.params.append(self.b_c)
+        
+        w_xc = self.w_xc
+        w_hc = self.w_hc
+        b_c = self.b_c
         
         self.w_xo = theano.shared(np.array(np.random.uniform(low=-bound_x, high=bound_x, size=(n_in, n_h)), dtype=theano.config.floatX), name=self.name+'w_xo', borrow=True)  # @UndefinedVariable
         self.params.append(self.w_xo)
@@ -53,51 +71,122 @@ class lstm(object):
         self.b_o = theano.shared(np.array(np.random.uniform(low=-bound_h, high=bound_h, size=(n_h,)), dtype=theano.config.floatX), name=self.name+'b_o', borrow=True)  # @UndefinedVariable
         self.params.append(self.b_o)
         
+        w_xo = self.w_xo
+        w_ho = self.w_ho
+        b_o = self.b_o
+        
         # self.w_y = theano.shared(np.array(np.random.uniform(low=-bound_x, high=bound_x, size=(n_h, n_out)), dtype=theano.config.floatX))  # @UndefinedVariable
         # self.params.append(self.w_y)
         # self.b_y = theano.shared(np.array(np.random.uniform(low=-bound_h, high=bound_h, size=(n_out,)), dtype=theano.config.floatX))  # @UndefinedVariable
         # self.params.append(self.b_y)
+        
+        if attention == 1:
+            self.att_wa = theano.shared(np.array(np.random.uniform(low=-bound_h, high=bound_h, size=(n_h, n_h)), dtype=theano.config.floatX), name=self.name+'att_wa', borrow=True)  # @UndefinedVariable
+            self.params.append(self.att_wa)
+            self.att_ua = theano.shared(np.array(np.random.uniform(low=-bound_h, high=bound_h, size=(n_h, n_h)), dtype=theano.config.floatX), name=self.name+'att_ua', borrow=True)  # @UndefinedVariable
+            self.params.append(self.att_ua)       
+            self.att_va = theano.shared(np.array(np.random.uniform(low=-bound_h, high=bound_h, size=(n_h, )), dtype=theano.config.floatX), name=self.name+'att_u', borrow=True)  # @UndefinedVariable
+            self.params.append(self.att_va)    
+            
+            att_wa = self.att_wa    
+            att_ua = self.att_ua   
+            att_va = self.att_va   
+        
+            self.w_coni = theano.shared(np.array(np.random.uniform(low=-bound_x, high=bound_x, size=(n_in, n_h)), dtype=theano.config.floatX), name=self.name+'w_coni', borrow=True)  # @UndefinedVariable
+            self.params.append(self.w_coni)
+            self.w_conf = theano.shared(np.array(np.random.uniform(low=-bound_x, high=bound_x, size=(n_in, n_h)), dtype=theano.config.floatX), name=self.name+'w_conf', borrow=True)  # @UndefinedVariable
+            self.params.append(self.w_conf)
+            self.w_conc = theano.shared(np.array(np.random.uniform(low=-bound_x, high=bound_x, size=(n_in, n_h)), dtype=theano.config.floatX), name=self.name+'w_conc', borrow=True)  # @UndefinedVariable
+            self.params.append(self.w_conc)
+            self.w_cono = theano.shared(np.array(np.random.uniform(low=-bound_x, high=bound_x, size=(n_in, n_h)), dtype=theano.config.floatX), name=self.name+'w_cono', borrow=True)  # @UndefinedVariable
+            self.params.append(self.w_cono)
+            
+            w_coni = self.w_coni
+            w_conf = self.w_conf
+            w_conc = self.w_conc
+            w_cono = self.w_cono
+        
+        
         
         self.c0 = theano.shared(np.array(np.zeros((n_h,)), dtype=theano.config.floatX), name=self.name+'c0', borrow=True)  # @UndefinedVariable
         self.params.append(self.c0)
         self.h0 = T.tanh(self.c0)
         self.h0.name = self.name+'h0'
         
+        if attention == 1:
+            def one_step(x, m_, H, c_t1, h_t1, tr_H):
+                
+                e = T.dot(T.tanh(T.dot(h_t1, att_wa).dimshuffle(0,'x',1)+tr_H),att_va)  #(batch_size, sen_len)
+                a = T.nnet.softmax(e)   #(batch_size, sen_len)
+                con = T.sum(a[:,:,None]*H,axis = -2)
+                i = T.nnet.sigmoid(T.dot(x, w_xi) + T.dot(h_t1, w_hi) + T.dot(con, w_coni) + b_i)  # + T.dot(c_t1, w_ci)
+                f = T.nnet.sigmoid(T.dot(x, w_xf) + T.dot(h_t1, w_hf) + T.dot(con, w_conf) + b_f)  # + T.dot(c_t1, w_cf)
+                c = f * c_t1 + i * T.tanh(T.dot(x, w_xc) + T.dot(h_t1, w_hc) + T.dot(con, w_conc) + b_c)
+                ct = m_[:, None] * c + (1 - m_)[:, None] * c_t1
+                o = T.nnet.sigmoid(T.dot(x, w_xo) + T.dot(h_t1, w_ho) + T.dot(con, w_cono) + b_o)  # + T.dot(ct, w_co)
+                h = o * T.tanh(ct)
+                ht = m_[:, None] * h + (1 - m_)[:, None] * h_t1
+                # y = T.nnet.sigmoid(T.dot(ht, w_y) + b_y)
+                return [T.cast(ct, dtype=theano.config.floatX), T.cast(ht, dtype=theano.config.floatX)]  # , y] @UndefinedVariable
+            self.normal_one_step = one_step
+            
+            def one_step_sentence(x, H, c_t1, h_t1, tr_H):
+                e = T.dot(T.tanh(T.dot(h_t1, att_wa).dimshuffle(0,'x',1)+tr_H),att_va)  #(batch_size, sen_len)
+                a = T.nnet.softmax(e)   #(batch_size, sen_len)
+                con = T.sum(a[:,:,None]*H,axis = -2)
+                i = T.nnet.sigmoid(T.dot(x, w_xi) + T.dot(h_t1, w_hi) + T.dot(con, w_coni) + b_i)  # + T.dot(c_t1, w_ci)
+                f = T.nnet.sigmoid(T.dot(x, w_xf) + T.dot(h_t1, w_hf) + T.dot(con, w_conf) + b_f)  # + T.dot(c_t1, w_cf)
+                c = f * c_t1 + i * T.tanh(T.dot(x, w_xc) + T.dot(h_t1, w_hc) + T.dot(con, w_conc) + b_c)
+                o = T.nnet.sigmoid(T.dot(x, w_xo) + T.dot(h_t1, w_ho) + T.dot(con, w_cono) + b_o)  # + T.dot(ct, w_co)
+                h = o * T.tanh(c)
+                return [c, h]  # , y]   , y]  
+            
+            self.sen2doc_one_step = one_step_sentence
+        else:
+            def one_step(x, m_, c_t1, h_t1):
+                i = T.nnet.sigmoid(T.dot(x, w_xi) + T.dot(h_t1, w_hi) + b_i)  # + T.dot(c_t1, w_ci)
+                f = T.nnet.sigmoid(T.dot(x, w_xf) + T.dot(h_t1, w_hf) + b_f)  # + T.dot(c_t1, w_cf)
+                c = f * c_t1 + i * T.tanh(T.dot(x, w_xc) + T.dot(h_t1, w_hc) + b_c)
+                ct = m_[:, None] * c + (1 - m_)[:, None] * c_t1
+                o = T.nnet.sigmoid(T.dot(x, w_xo) + T.dot(h_t1, w_ho) + b_o)  # + T.dot(ct, w_co)
+                h = o * T.tanh(ct)
+                ht = m_[:, None] * h + (1 - m_)[:, None] * h_t1
+                # y = T.nnet.sigmoid(T.dot(ht, w_y) + b_y)
+                return [T.cast(ct, dtype=theano.config.floatX), T.cast(ht, dtype=theano.config.floatX)]  # , y] @UndefinedVariable
+            self.normal_one_step = one_step
+            
+            def one_step_sentence(x, c_t1, h_t1):
+                i = T.nnet.sigmoid(T.dot(x , w_xi) + T.dot(h_t1, w_hi) + b_i)  # + T.dot(c_t1, w_ci) 
+                f = T.nnet.sigmoid(T.dot(x, w_xf) + T.dot(h_t1, w_hf) + b_f)  # + T.dot(c_t1, w_cf)
+                c = f * c_t1 + i * T.tanh(T.dot(x, w_xc) + T.dot(h_t1, w_hc) + b_c)
+                o = T.nnet.sigmoid(T.dot(x, w_xo) + T.dot(h_t1, w_ho) + b_o)  # + T.dot(c, w_co)
+                h = o * T.tanh(c)
+                return [c, h]  # , y]   , y]  
+            
+            self.sen2doc_one_step = one_step_sentence
+            
         
-        def one_step(x, m_, c_t1, h_t1, w_xi, w_hi, b_i, w_xf, w_hf, b_f, w_xc, w_hc, b_c, w_xo, w_ho, b_o):
-            i = T.nnet.sigmoid(T.dot(x, w_xi) + T.dot(h_t1, w_hi) + b_i)  # + T.dot(c_t1, w_ci)
-            f = T.nnet.sigmoid(T.dot(x, w_xf) + T.dot(h_t1, w_hf) + b_f)  # + T.dot(c_t1, w_cf)
-            c = f * c_t1 + i * T.tanh(T.dot(x, w_xc) + T.dot(h_t1, w_hc) + b_c)
-            ct = m_[:, None] * c + (1 - m_)[:, None] * c_t1
-            o = T.nnet.sigmoid(T.dot(x, w_xo) + T.dot(h_t1, w_ho) + b_o)  # + T.dot(ct, w_co)
-            h = o * T.tanh(ct)
-            ht = m_[:, None] * h + (1 - m_)[:, None] * h_t1
-            # y = T.nnet.sigmoid(T.dot(ht, w_y) + b_y)
-            return [T.cast(ct, dtype=theano.config.floatX), T.cast(ht, dtype=theano.config.floatX)]  # , y] @UndefinedVariable
         
-        self.normal_one_step = one_step
-        
-
-        def one_step_sentence(x_t, c_t1, h_t1, w_xi, w_hi, b_i, w_xf, w_hf, b_f, w_xc, w_hc, b_c, w_xo, w_ho, b_o):
-            i = T.nnet.sigmoid(T.dot(x_t , w_xi) + T.dot(h_t1, w_hi) + b_i)  # + T.dot(c_t1, w_ci) 
-            f = T.nnet.sigmoid(T.dot(x_t, w_xf) + T.dot(h_t1, w_hf) + b_f)  # + T.dot(c_t1, w_cf)
-            c = f * c_t1 + i * T.tanh(T.dot(x_t, w_xc) + T.dot(h_t1, w_hc) + b_c)
-            o = T.nnet.sigmoid(T.dot(x_t, w_xo) + T.dot(h_t1, w_ho) + b_o)  # + T.dot(c, w_co)
-            h = o * T.tanh(c)
-            return [c, h]  # , y]   , y]  
-        
-        self.sen2doc_one_step = one_step_sentence
-        
-        
-    def get_sentence_output(self,emb_x,mask):
-        [cs, hs], _ = theano.scan(fn=self.normal_one_step, sequences=[emb_x, mask], outputs_info=[T.alloc(self.c0, emb_x.shape[1], self.n_h), T.alloc(self.h0, emb_x.shape[1], self.n_h)], non_sequences=self.params[:-1])  # , None])
+    def get_sentence_output(self,emb_x,mask, H = None):
+        if self.attention != 0:
+            H = H.dimshuffle(1,0,2)    
+            tr_H = T.dot(H, self.att_ua)
+            [cs, hs], _ = theano.scan(fn=self.normal_one_step, sequences=[emb_x, mask, H], outputs_info=[T.alloc(self.c0, emb_x.shape[1], self.n_h), T.alloc(self.h0, emb_x.shape[1], self.n_h)],non_sequences=[tr_H])  # , None])
+        else:
+            [cs, hs], _ = theano.scan(fn=self.normal_one_step, sequences=[emb_x, mask], outputs_info=[T.alloc(self.c0, emb_x.shape[1], self.n_h), T.alloc(self.h0, emb_x.shape[1], self.n_h)])  # , None])
+            
         result = {}
         result['hs'] = hs
         result['cs'] = cs
         return result
     
-    def get_onedoc_output(self,x):        
-        [cs, hs], _ = theano.scan(fn=self.sen2doc_one_step, sequences=x, outputs_info=[self.c0, self.h0], non_sequences=self.params[:-1])  # , None])
+    def get_onedoc_output(self,x, H = None):
+        if self.attention != 0:    
+            H = H.dimshuffle(1,0,2)              #now H (batch_size, sen_len, n_dim)    
+            tr_H = T.dot(H, self.att_ua)  #tr_H (batch_size, sen_len, n_dim)       
+            [cs, hs], _ = theano.scan(fn=self.sen2doc_one_step, sequences=[x, H], outputs_info=[self.c0, self.h0],non_sequences=[tr_H])  # , None])
+        else:
+            [cs, hs], _ = theano.scan(fn=self.sen2doc_one_step, sequences=x, outputs_info=[self.c0, self.h0])  # , None])
         result = {}
         result['hs'] = hs
         result['cs'] = cs
